f8a962ee683a1fecfedb29e0d3e58753a9752c83
钱萌 <qianmeng@qianmengdembp.kingsoft.cn>
2017-09-01 16:51:36 +0800

    vue无限递归

---
+++ src/components/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <Tree :data="data" @nodeClick="nCllick"></Tree>
+  </div>
+</template>
+
+<script>
+  import Tree from '@/components/tree.vue'
+
+  export default {
+    components: {Tree},
+    name: 'app',
+    data () {
+      return {
+        data: {
+          'title': '111',
+          'children': [
+            {
+              'title': '111 - 1'
+            }, {
+              'title': '111 - 2',
+              'children': [
+                {
+                  'title': '111 - 2 - 1',
+                  'children': [
+                    {
+                      'title': '111 - 2 - 1 - 1'
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      }
+    },
+    methods: {
+      nCllick (title) {
+        console.log(title)
+      }
+    }
+
+  }
+</script>
---
+++ src/components/tree.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="tree">
+    <li>
+      <span v-html="data.title"></span>
+      <m-tree v-for="(item, idx) in data.children" :data="item" :key="idx"></m-tree>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'mTree',
+    props: {
+      data: {
+        type: Object,
+        default () {
+          return {}
+        }
+      }
+    }
+  }
+</script>
